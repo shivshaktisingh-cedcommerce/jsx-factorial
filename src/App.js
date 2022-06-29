7f71418f001750c61b9caa75a28b0c238ee348fc
shivshaktisingh-cedcommerce <shivshaktisingh@cedcoss.com>
2022-06-29 13:04:15 +0530

    factorial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+ var number =8;
+function App() {
+  var txt=1;
+ for(let i=number;i>1;i--){
+    txt=txt*i;
+ }
+  return (
+    <div className="App">
+     <h1>{txt}</h1>
+    </div>
+  );
+}
+
+export default App;
